a528223c4064adb232a0053c924da1c88f07b7a9
aizealawin <ktmudalige@gmail.com>
2023-03-15 14:54:29 -0400

    Spring Boot Project Init

---
+++ compliment/src/main/java/com/example/compliment/ComplimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.compliment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplimentApplication.class, args);
+	}
+
+}
