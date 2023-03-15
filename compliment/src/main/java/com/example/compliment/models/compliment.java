8bfcf9009bff5a5b62b5c80d9dc7eebc111321d1
aizealawin <ktmudalige@gmail.com>
2023-03-15 18:33:21 -0400

    deleted accidental imports

--- compliment/src/main/java/com/example/compliment/models/compliment.java
+++ compliment/src/main/java/com/example/compliment/models/compliment.java
@@ -1,7 +1,5 @@
 package com.example.compliment.models;
 
-import org.springframework.web.servlet.FlashMapManager;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
