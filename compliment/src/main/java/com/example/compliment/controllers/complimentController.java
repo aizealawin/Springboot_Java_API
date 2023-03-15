b39d14264b57c3150b810da830fff51d7e2966a8
aizealawin <ktmudalige@gmail.com>
2023-03-15 16:45:01 -0400

    Post route init for compliments

--- compliment/src/main/java/com/example/compliment/controllers/complimentController.java
+++ compliment/src/main/java/com/example/compliment/controllers/complimentController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.compliment.exceptions.ResourceNotFoundException;
 import com.example.compliment.models.compliment;
 import com.example.compliment.repository.complimentRepository;
+
+import jakarta.persistence.Entity;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -35,7 +39,14 @@ public class complimentController {
         .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + complimentId));
     return ResponseEntity.ok().body(compliment);
   }
+
   // save compliment
+  @PostMapping("compliments")
+  public compliment createCompliment(@RequestBody compliment compliment) {
+    return this.complimentRepository.save(compliment);
+
+  }
+
   // update compliment
   // delete compliment
 
